5f6d2f15d44d2ee3e15c3d9f2971fac1e5e7f75f
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 18:55:06 +0700

    Referenced content and weather entry

--- Assets/WebClient/Code/UI/Screens/WeatherScreen/View/UnityUIWeatherScreenView.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/View/UnityUIWeatherScreenView.cs
@@ -1,6 +1,13 @@
-﻿namespace WebClient
+﻿using UnityEngine;
+
+namespace WebClient
 {
     public sealed class UnityUIWeatherScreenView : WeatherScreenViewBase
     {
+        [SerializeField]
+        private RectTransform _content;
+
+        [SerializeField]
+        private WeatherEntry _weatherEntryTemplate;
     }
 }
