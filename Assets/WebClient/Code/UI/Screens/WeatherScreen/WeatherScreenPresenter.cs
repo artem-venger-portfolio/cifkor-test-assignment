bda352425c8f3d1a9976c8799035284fada302c5
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 17:02:41 +0700

    Add request coroutine

--- Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenPresenter.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenPresenter.cs
@@ -1,14 +1,21 @@
-﻿namespace WebClient
+﻿using System.Collections;
+using UnityEngine;
+
+namespace WebClient
 {
     public class WeatherScreenPresenter
     {
         private readonly WeatherScreenModel _model;
         private readonly WeatherScreenViewBase _view;
+        private readonly MonoBehaviourFunctions _monoBehaviourFunctions;
+        private Coroutine _requestCoroutine;
 
-        public WeatherScreenPresenter(WeatherScreenModel model, WeatherScreenViewBase view)
+        public WeatherScreenPresenter(WeatherScreenModel model, WeatherScreenViewBase view,
+                                      MonoBehaviourFunctions monoBehaviourFunctions)
         {
             _model = model;
             _view = view;
+            _monoBehaviourFunctions = monoBehaviourFunctions;
 
             _model.IsOpenChanged += InOpenChangedEventHandler;
         }
@@ -18,10 +25,23 @@
             if (isOpen)
             {
                 _view.Open();
+                _requestCoroutine = _monoBehaviourFunctions.RunCoroutine(GetRequestCoroutine());
             }
             else
             {
                 _view.Close();
+                _monoBehaviourFunctions.KillCoroutine(_requestCoroutine);
+            }
+        }
+
+        private IEnumerator GetRequestCoroutine()
+        {
+            const float data_update_time = 5f;
+
+            while (true)
+            {
+                Debug.Log(message: "Update data");
+                yield return new WaitForSeconds(data_update_time);
             }
         }
     }
