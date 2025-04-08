2d90969eb89661775b105f940099339f05ef9286
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 00:48:30 +0700

    Remove unneeded fields

--- Assets/WebClient/Code/TabsScene/TabsSceneReferences.cs
+++ Assets/WebClient/Code/TabsScene/TabsSceneReferences.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace WebClient
@@ -8,20 +7,10 @@ namespace WebClient
     public class TabsSceneReferences
     {
         [SerializeField]
-        private Component[] _screenViews;
-
-        [SerializeField]
-        private TabToName[] _tabToNameCollection;
-
-        [SerializeField]
         private NavigationPanelViewBase _navigationPanelView;
 
         [SerializeField]
         private WeatherScreenViewBase _weatherTabViewTemplate;
-
-        public Component[] ScreenViews => _screenViews;
-
-        public IList<TabToName> TabToNameCollection => _tabToNameCollection;
 
         public NavigationPanelViewBase NavigationPanelView => _navigationPanelView;
 
