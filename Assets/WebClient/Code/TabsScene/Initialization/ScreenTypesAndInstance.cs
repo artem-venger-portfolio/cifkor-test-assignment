14222f670dbcf275f088cf3414ebcae17d92af8f
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 14:59:23 +0700

    Create container for screen types and instances

---
+++ Assets/WebClient/Code/TabsScene/Initialization/ScreenTypesAndInstance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WebClient
+{
+    public readonly struct ScreenTypesAndInstance
+    {
+        private ScreenTypesAndInstance(Type model, Type viewType, Type presenter, ViewBase viewInstance)
+        {
+            Model = model;
+            ViewType = viewType;
+            Presenter = presenter;
+            ViewInstance = viewInstance;
+        }
+
+        public Type Model { get; }
+        public Type ViewType { get; }
+        public Type Presenter { get; }
+        public ViewBase ViewInstance { get; }
+
+        public static ScreenTypesAndInstance Create<TModel, TView, TPresenter>(TView viewInstance) where TView: ViewBase
+        {
+            return new ScreenTypesAndInstance(typeof(TModel), typeof(TView), typeof(TPresenter), viewInstance);
+        }
+    }
+}
