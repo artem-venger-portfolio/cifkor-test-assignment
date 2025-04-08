e2098334b7c381851c069e6326c79b500625424c
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 18:45:17 +0700

    Create weather entry

---
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/View/WeatherEntry.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WebClient
+{
+    public class WeatherEntry : MonoBehaviour
+    {
+        [SerializeField]
+        private RawImage _image;
+
+        [SerializeField]
+        private TMP_Text _temperatureField;
+
+        public void SetTexture(Texture2D texture)
+        {
+            _image.texture = texture;
+        }
+
+        public void SetTemperature(float value, string unit)
+        {
+            _temperatureField.text = value + unit;
+        }
+    }
+}
