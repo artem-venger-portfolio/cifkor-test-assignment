95f52aed1a33eb5af60e7e622561bf67f8a47fba
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 23:41:04 +0700

    Subscribed to tab added event

--- Assets/WebClient/Code/UI/Screens/NavigationPanel/NavigationPanelPresenter.cs
+++ Assets/WebClient/Code/UI/Screens/NavigationPanel/NavigationPanelPresenter.cs
@@ -17,6 +17,8 @@ namespace WebClient
             _dogBreedsScreen = dogBreedsScreen;
             _weatherScreen = weatherScreen;
 
+            _model.TabAdded += TabAddedEventHandler;
+
             _view.WeatherTabSelected += WeatherTabSelectedEventHandler;
             _view.DogBreedsTabSelected += DogBreedsTabSelectedEventHandler;
         }
@@ -31,6 +33,10 @@ namespace WebClient
         {
             LogInfo(nameof(DogBreedsTabSelectedEventHandler));
             _weatherScreen.Close();
+        }
+
+        private void TabAddedEventHandler(NavigationPanelTabModelBase tabModel)
+        {
         }
 
         private void LogInfo(string message)
