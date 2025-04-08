43a1c6dba62861a5ccfb69e81cbd05befeeaef5d
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 21:19:37 +0700

    Add UsedImplicitly attribute

--- Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenPresenter.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenPresenter.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace WebClient
 {
+    [UsedImplicitly]
     public class WeatherScreenPresenter
     {
         private readonly WeatherScreenModel _model;
