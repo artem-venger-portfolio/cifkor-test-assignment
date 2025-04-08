c2857f6c6800fe672373c9d845dde570ed192ec0
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 14:17:39 +0700

    Create navigation panel view

--- Assets/WebClient/Code/UI/Screens/NavigationPanel/View/UnityUINavigationPanelView.cs
+++ Assets/WebClient/Code/UI/Screens/NavigationPanel/View/UnityUINavigationPanelView.cs
@@ -1,6 +1,14 @@
-﻿namespace WebClient
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace WebClient
 {
     public sealed class UnityUINavigationPanelView : NavigationPanelViewBase
     {
+        [SerializeField]
+        private Toggle _weatherToggle;
+
+        [SerializeField]
+        private Toggle _dogBreedsToggle;
     }
 }
