753d15316a8ec01ca7a1a79a1fa4163890d699d6
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 04:16:19 +0700

    Add methods to texture cache

--- Assets/WebClient/Code/RequestQueue/TextureCache/ITextureCache.cs
+++ Assets/WebClient/Code/RequestQueue/TextureCache/ITextureCache.cs
@@ -1,6 +1,10 @@
-﻿namespace WebClient
+﻿using UnityEngine;
+
+namespace WebClient
 {
     public interface ITextureCache
     {
+        void Save(string key, Texture2D texture);
+        bool Contains(string key);
     }
 }
--- Assets/WebClient/Code/RequestQueue/TextureCache/TextureCache.cs
+++ Assets/WebClient/Code/RequestQueue/TextureCache/TextureCache.cs
@@ -1,6 +1,20 @@
-﻿namespace WebClient
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WebClient
 {
     public class TextureCache : ITextureCache
     {
+        private readonly Dictionary<string, Texture2D> _keyToTexture;
+
+        public void Save(string key, Texture2D texture)
+        {
+            _keyToTexture.Add(key, texture);
+        }
+
+        public bool Contains(string key)
+        {
+            return _keyToTexture.ContainsKey(key);
+        }
     }
 }
