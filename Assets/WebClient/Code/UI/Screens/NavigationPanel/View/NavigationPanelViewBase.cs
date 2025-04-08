decad53dd43ebf1e184ab8c2feaef50e44a5f5ef
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 01:21:12 +0700

    Remove unneeded abstract class

--- Assets/WebClient/Code/UI/Screens/NavigationPanel/View/NavigationPanelViewBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace WebClient
-{
-    public abstract class NavigationPanelViewBase : MonoBehaviour, INavigationPanelView
-    {
-        public abstract void AddTab(string tabName, UnityUINavigationPanelTabViewBase tab);
-    }
-}
--- Assets/WebClient/Code/UI/Screens/NavigationPanel/View/UnityUINavigationPanelView.cs
+++ Assets/WebClient/Code/UI/Screens/NavigationPanel/View/UnityUINavigationPanelView.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 namespace WebClient
 {
-    public sealed class UnityUINavigationPanelView : NavigationPanelViewBase
+    public sealed class UnityUINavigationPanelView : MonoBehaviour, INavigationPanelView
     {
         [SerializeField]
         private ToggleGroup _toggleGroup;
@@ -14,7 +14,7 @@ namespace WebClient
         [SerializeField]
         private Transform _tabContainer;
 
-        public override void AddTab(string tabName, UnityUINavigationPanelTabViewBase tab)
+        public void AddTab(string tabName, UnityUINavigationPanelTabViewBase tab)
         {
             tab.SetParent(_tabContainer);
             var toggle = Instantiate(_toggleTemplate, _toggleGroup.transform, worldPositionStays: false);
