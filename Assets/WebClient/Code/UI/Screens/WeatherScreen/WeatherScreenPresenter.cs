d8c3bedfbed3bd0ea2d8653a256551f872eb2445
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 18:19:14 +0700

    Load all textures

--- Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenPresenter.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenPresenter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -9,6 +10,8 @@ namespace WebClient
         private readonly WeatherScreenModel _model;
         private readonly WeatherScreenViewBase _view;
         private readonly MonoBehaviourFunctions _monoBehaviourFunctions;
+        private readonly List<UnityWebRequest> _textureLoadingRequest;
+        private readonly Dictionary<string, Texture2D> _urlToTexture;
         private Coroutine _requestCoroutine;
 
         public WeatherScreenPresenter(WeatherScreenModel model, WeatherScreenViewBase view,
@@ -17,6 +20,8 @@ namespace WebClient
             _model = model;
             _view = view;
             _monoBehaviourFunctions = monoBehaviourFunctions;
+            _textureLoadingRequest = new List<UnityWebRequest>();
+            _urlToTexture = new Dictionary<string, Texture2D>();
 
             _model.IsOpenChanged += InOpenChangedEventHandler;
         }
@@ -59,7 +64,7 @@ namespace WebClient
                             break;
                         case UnityWebRequest.Result.Success:
                             Debug.Log(message: "Success");
-                            DisplayWeather(webRequest.downloadHandler.text);
+                            yield return DisplayWeather(webRequest.downloadHandler.text);
                             break;
                     }
                 }
@@ -68,13 +73,50 @@ namespace WebClient
             }
         }
 
-        private void DisplayWeather(string json)
+        private IEnumerator DisplayWeather(string json)
         {
             var response = JsonUtility.FromJson<WeatherResponse>(json);
             foreach (var currentPeriod in response.properties.periods)
             {
-                Debug.Log($"Icon: {currentPeriod.icon}, Temperature: {currentPeriod.temperature}{currentPeriod.temperatureUnit}");
+                var textureLoadingRequest = UnityWebRequestTexture.GetTexture(currentPeriod.icon);
+                textureLoadingRequest.SendWebRequest();
+                _textureLoadingRequest.Add(textureLoadingRequest);
             }
+
+            while (IsAllTexturesLoaded() == false)
+            {
+                yield return null;
+            }
+
+            foreach (var loadingRequest in _textureLoadingRequest)
+            {
+                if (loadingRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError(loadingRequest.error);
+                }
+                else
+                {
+                    var texture = DownloadHandlerTexture.GetContent(loadingRequest);
+                    var url = loadingRequest.url;
+                    _urlToTexture.Add(url, texture);
+                    Debug.Log($"{url} loaded");
+                }
+            }
+        }
+
+        private bool IsAllTexturesLoaded()
+        {
+            foreach (var currentRequest in _textureLoadingRequest)
+            {
+                if (currentRequest.isDone)
+                {
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
