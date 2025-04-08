940a2e202658bb1b5a57fa5dcc23392ba1e117f5
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 02:05:40 +0700

    Remove manually created tab and navigation panel

--- Assets/WebClient/Code/TabsScene/Initialization/TabsSceneInitialization.cs
+++ Assets/WebClient/Code/TabsScene/Initialization/TabsSceneInitialization.cs
@@ -17,19 +17,6 @@ namespace WebClient
         {
             _sceneContext = SceneContext.Create();
 
-            var monoBehaviourFunctionsGO = new GameObject(nameof(MonoBehaviourFunctions));
-            var monoBehaviourFunctions = monoBehaviourFunctionsGO.AddComponent<MonoBehaviourFunctions>();
-
-            var weatherTabModel = new WeatherScreenModel(monoBehaviourFunctions);
-            var weatherTabView = Object.Instantiate(_sceneReferences.WeatherTabViewTemplate);
-            var weatherTabPresenter = new WeatherScreenPresenter(weatherTabModel, weatherTabView);
-
-            var navigationPanelModel = new NavigationPanelModel();
-            var navigationPanelView = _sceneReferences.NavigationPanelView;
-            var navigationPanelPresenter = new NavigationPanelPresenter(navigationPanelModel, navigationPanelView);
-
-            navigationPanelView.AddTab(tabName: "Weather", weatherTabView);
-
             var installers = new InstallerBase[]
             {
                 new TabsSceneInstaller(_sceneReferences),
