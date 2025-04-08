92bcc442def9599f767664f84a271e0eb3202b67
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 15:46:12 +0700

    Remove screens information assembling logic from TabsSceneInitialization

--- Assets/WebClient/Code/TabsScene/Initialization/TabsSceneInitialization.cs
+++ Assets/WebClient/Code/TabsScene/Initialization/TabsSceneInitialization.cs
@@ -1,14 +1,10 @@
-﻿using System.Linq;
-using Zenject;
+﻿using Zenject;
 
 namespace WebClient
 {
     public class TabsSceneInitialization
     {
         private readonly TabsSceneReferences _sceneReferences;
-
-        private ScreenTypesAndInstance[] _screenTypesAndInstances;
-        private ViewBase[] _screenViews;
         private SceneContext _sceneContext;
 
         public TabsSceneInitialization(TabsSceneReferences sceneReferences)
@@ -18,8 +14,6 @@ namespace WebClient
 
         public void Perform()
         {
-            FindAndRecordScreenInstances();
-
             _sceneContext = SceneContext.Create();
 
             var installers = new InstallerBase[]
@@ -33,34 +27,6 @@ namespace WebClient
             }
 
             _sceneContext.Run();
-        }
-
-        private void FindAndRecordScreenInstances()
-        {
-            _screenViews = FindScreenViews();
-
-            var screenTypes = new[]
-            {
-                MVPTypesGroup.Create<NavigationPanelModel, NavigationPanelViewBase, NavigationPanelPresenter>(),
-            };
-
-            _screenTypesAndInstances = new ScreenTypesAndInstance[screenTypes.Length];
-            for (var i = 0; i < screenTypes.Length; i++)
-            {
-                _screenTypesAndInstances[i] = LinkScreenTypesWithViewInstance(screenTypes[i]);
-            }
-        }
-
-        private ViewBase[] FindScreenViews()
-        {
-            return _sceneReferences.ScreensContainer.GetComponentsInChildren<ViewBase>(includeInactive: true);
-        }
-
-        private ScreenTypesAndInstance LinkScreenTypesWithViewInstance(MVPTypesGroup group)
-        {
-            var viewType = group.View;
-            var viewInstance = _screenViews.First(v => viewType.IsAssignableFrom(v.GetType()));
-            return new ScreenTypesAndInstance(group.Model, viewType, group.Presenter, viewInstance);
         }
     }
 }
