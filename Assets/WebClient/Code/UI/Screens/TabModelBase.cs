3a3c573c26859ec8324a6493307e76b1ec5a3b07
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 20:55:26 +0700

    Move model open close logic to the base class

---
+++ Assets/WebClient/Code/UI/Screens/TabModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WebClient
+{
+    public abstract class TabModelBase
+    {
+        private bool _isOpen;
+
+        public bool IsOpen
+        {
+            get => _isOpen;
+            private set
+            {
+                if (_isOpen == value)
+                {
+                    return;
+                }
+
+                _isOpen = value;
+                IsOpenChanged?.Invoke(_isOpen);
+            }
+        }
+
+        public event Action<bool> IsOpenChanged;
+
+        public void Open()
+        {
+            IsOpen = true;
+        }
+
+        public void Close()
+        {
+            IsOpen = false;
+        }
+    }
+}
--- Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenModel.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenModel.cs
@@ -1,36 +1,7 @@
-﻿using System;
-
-namespace WebClient
+﻿namespace WebClient
 {
-    public class WeatherScreenModel
+    public class WeatherScreenModel : TabModelBase
     {
-        private bool _isOpen;
-
-        public bool IsOpen
-        {
-            get => _isOpen;
-            private set
-            {
-                if (_isOpen == value)
-                {
-                    return;
-                }
-
-                _isOpen = value;
-                IsOpenChanged?.Invoke(_isOpen);
-            }
-        }
-
-        public event Action<bool> IsOpenChanged;
-
-        public void Open()
-        {
-            IsOpen = true;
-        }
-
-        public void Close()
-        {
-            IsOpen = false;
-        }
+        
     }
 }
