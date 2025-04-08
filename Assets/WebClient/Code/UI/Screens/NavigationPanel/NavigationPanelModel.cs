54e8157b10b9df54b5e107198f3b809784fb6e8e
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 22:51:25 +0700

    Add AddTab method to model

--- Assets/WebClient/Code/UI/Screens/NavigationPanel/NavigationPanelModel.cs
+++ Assets/WebClient/Code/UI/Screens/NavigationPanel/NavigationPanelModel.cs
@@ -1,6 +1,18 @@
-﻿namespace WebClient
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebClient
 {
     public class NavigationPanelModel
     {
+        private readonly List<string> _tabs = new();
+
+        public event Action<string> TabAdded;
+
+        public void AddTab(string name)
+        {
+            _tabs.Add(name);
+            TabAdded?.Invoke(name);
+        }
     }
 }
