3aa1e28c9797487b2dcd99968f338111a022a132
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 20:56:44 +0700

    Copy field used for requests to model

--- Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenModel.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenModel.cs
@@ -1,10 +1,24 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace WebClient
 {
     [UsedImplicitly]
     public class WeatherScreenModel : TabModelBase
     {
-        
+        private readonly MonoBehaviourFunctions _monoBehaviourFunctions;
+        private readonly List<UnityWebRequest> _textureLoadingRequest;
+        private readonly Dictionary<string, Texture2D> _urlToTexture;
+        private readonly List<WeatherPeriod> _periods;
+
+        public WeatherScreenModel(MonoBehaviourFunctions monoBehaviourFunctions)
+        {
+            _monoBehaviourFunctions = monoBehaviourFunctions;
+            _textureLoadingRequest = new List<UnityWebRequest>();
+            _urlToTexture = new Dictionary<string, Texture2D>();
+            _periods = new List<WeatherPeriod>();
+        }
     }
 }
