23368672addd7ede97722cb4400f1d08822577a6
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 04:37:38 +0700

    Implement WeatherRequest

---
+++ Assets/WebClient/Code/RequestQueue/WeatherRequest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace WebClient
+{
+    public class WeatherRequest : IWebRequest
+    {
+        private const string URL = "https://api.weather.gov/gridpoints/TOP/32,81/forecast";
+        private readonly MonoBehaviourFunctions _monoBehaviourFunctions;
+        private readonly ITextureLoader _textureLoader;
+        private readonly ITextureCache _textureCache;
+        private UnityWebRequest _weatherRequest;
+        private Coroutine _requestCoroutine;
+        private bool _isDone;
+
+        public WeatherRequest(MonoBehaviourFunctions monoBehaviourFunctions, ITextureLoader textureLoader,
+                              ITextureCache textureCache)
+        {
+            _monoBehaviourFunctions = monoBehaviourFunctions;
+            _textureLoader = textureLoader;
+            _textureCache = textureCache;
+        }
+
+        public RequestType Type => RequestType.Weather;
+
+        public void Send()
+        {
+            _requestCoroutine = _monoBehaviourFunctions.RunCoroutine(GetRequestEnumerator());
+        }
+
+        public bool IsDone()
+        {
+            return _isDone;
+        }
+
+        public void Finish()
+        {
+            _weatherRequest.Dispose();
+            _weatherRequest = null;
+        }
+
+        private IEnumerator GetRequestEnumerator()
+        {
+            _isDone = false;
+            _weatherRequest = UnityWebRequest.Get(URL);
+
+            yield return _weatherRequest.SendWebRequest();
+
+            if (_weatherRequest.result != UnityWebRequest.Result.Success)
+            {
+                _requestCoroutine = null;
+                _isDone = true;
+                yield break;
+            }
+
+            var json = _weatherRequest.downloadHandler.text;
+            var response = JsonUtility.FromJson<WeatherResponse>(json);
+            var responsePeriods = response.properties.periods;
+
+            foreach (var currentPeriod in responsePeriods)
+            {
+                var iconURL = currentPeriod.icon;
+                if (IsTextureLoadingOrLoaded(iconURL))
+                {
+                    continue;
+                }
+
+                _textureLoader.StartNew(iconURL);
+            }
+
+            while (_textureLoader.IsAllLoaded() == false)
+            {
+                yield return null;
+            }
+
+            _textureLoader.Finish();
+            _requestCoroutine = null;
+            _isDone = true;
+        }
+
+        private bool IsTextureLoadingOrLoaded(string url)
+        {
+            return _textureLoader.IsLoading(url) || _textureCache.Contains(url);
+        }
+    }
+}
