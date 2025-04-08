ba80e2f7ddfb6f5f328846866eb389a0c03dfc16
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 19:04:29 +0700

    Create struct with weather period data

---
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherPeriod.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace WebClient
+{
+    public readonly struct WeatherPeriod
+    {
+        public WeatherPeriod(Texture texture, float temperature, string unit)
+        {
+            Texture = texture;
+            Temperature = temperature;
+            Unit = unit;
+        }
+
+        private Texture Texture { get; }
+        private float Temperature { get; }
+        private string Unit { get; }
+    }
+}
