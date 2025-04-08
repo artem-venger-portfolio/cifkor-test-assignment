41b6836af34d1a6067bd85271817f0579246a7f2
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 23:38:40 +0700

    Add manually created weather tab to manually created navigation panel

--- Assets/WebClient/Code/TabsScene/Initialization/TabsSceneInitialization.cs
+++ Assets/WebClient/Code/TabsScene/Initialization/TabsSceneInitialization.cs
@@ -27,6 +27,8 @@ namespace WebClient
             var navigationPanelPresenter = new NavigationPanelPresenter(navigationPanelModel, navigationPanelView,
                                                                         dogBreedsScreen: null, weatherTabModel);
 
+            navigationPanelModel.AddTab(weatherTabModel);
+
             var installers = new InstallerBase[]
             {
                 new ScreensInstaller(_sceneReferences),
