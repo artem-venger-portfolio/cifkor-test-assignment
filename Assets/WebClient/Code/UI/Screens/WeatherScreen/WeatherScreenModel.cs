b93aab5c05f708b57177d1473a75e84ca8fd9423
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 16:38:29 +0700

    Add methods to open and close weather screen

--- Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenModel.cs
+++ Assets/WebClient/Code/UI/Screens/WeatherScreen/WeatherScreenModel.cs
@@ -1,6 +1,36 @@
-﻿namespace WebClient
+﻿using System;
+
+namespace WebClient
 {
     public class WeatherScreenModel
     {
+        private bool _isOpen;
+
+        public bool IsOpen
+        {
+            get => _isOpen;
+            private set
+            {
+                if (_isOpen == value)
+                {
+                    return;
+                }
+
+                _isOpen = value;
+                IsOpenChanged?.Invoke(_isOpen);
+            }
+        }
+
+        public event Action<bool> IsOpenChanged;
+
+        public void Open()
+        {
+            IsOpen = true;
+        }
+
+        public void Close()
+        {
+            IsOpen = false;
+        }
     }
 }
