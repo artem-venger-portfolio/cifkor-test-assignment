c74ff3eda412d27922e297e3652cffaa09540ebe
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 23:28:34 +0700

    Add open logic to base tab model

--- Assets/WebClient/Code/UI/Screens/NavigationPanel/Tab/NavigationPanelTabModelBase.cs
+++ Assets/WebClient/Code/UI/Screens/NavigationPanel/Tab/NavigationPanelTabModelBase.cs
@@ -1,6 +1,48 @@
-﻿namespace WebClient
+﻿using System;
+
+namespace WebClient
 {
     public abstract class NavigationPanelTabModelBase
     {
+        private bool _isOpen;
+
+        public event Action<bool> IsOpenChanged;
+
+        public void Open()
+        {
+            if (_isOpen)
+            {
+                return;
+            }
+
+            _isOpen = true;
+            OnOpen();
+            InvokeIsOpenChanged();
+        }
+
+        public void Close()
+        {
+            if (_isOpen == false)
+            {
+                return;
+            }
+
+            _isOpen = false;
+            OnClose();
+            InvokeIsOpenChanged();
+        }
+
+        protected virtual void OnOpen()
+        {
+        }
+
+        protected virtual void OnClose()
+        {
+        }
+
+        private void InvokeIsOpenChanged()
+        {
+            IsOpenChanged?.Invoke(_isOpen);
+        }
     }
 }
