41a58aafd0d23b6a6363b964d762e727cbb4b50b
Venger Artem <artem.venger.professional@gmail.com>
2025-04-08 22:15:25 +0700

    Create toggle component

---
+++ Assets/WebClient/Code/UI/Screens/NavigationPanel/View/NavigationPanelToggle.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace WebClient
+{
+    public class NavigationPanelToggle : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _pageNameField;
+
+        public void SetPageName(string pageName)
+        {
+            _pageNameField.text = pageName;
+        }
+    }
+}
