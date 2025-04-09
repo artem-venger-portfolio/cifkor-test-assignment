15bc11783565bb81924ad3d9a2682d8982552eb5
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 09:38:46 +0700

    Send description request

--- Assets/WebClient/Code/UI/Screens/DogBreedsScreen/DogBreedsScreenModel.cs
+++ Assets/WebClient/Code/UI/Screens/DogBreedsScreen/DogBreedsScreenModel.cs
@@ -5,14 +5,17 @@ namespace WebClient
 {
     public class DogBreedsScreenModel
     {
+        private readonly DogBreedDescriptionRequest.Factory _descriptionRequestFactory;
         private readonly DogBreedsRequest.Factory _dogBreedsRequestFactory;
         private readonly IRequestQueue _requestQueue;
         private readonly IProjectLogger _logger;
 
-        public DogBreedsScreenModel(DogBreedsRequest.Factory dogBreedsRequestFactory, IRequestQueue requestQueue,
-                                    IProjectLogger logger)
+        public DogBreedsScreenModel(DogBreedsRequest.Factory dogBreedsRequestFactory,
+                                    DogBreedDescriptionRequest.Factory descriptionRequestFactory,
+                                    IRequestQueue requestQueue, IProjectLogger logger)
         {
             _dogBreedsRequestFactory = dogBreedsRequestFactory;
+            _descriptionRequestFactory = descriptionRequestFactory;
             _requestQueue = requestQueue;
             _logger = logger;
         }
@@ -33,6 +36,14 @@ namespace WebClient
 
         public void GetBreedDescription(int index)
         {
+            var id = Breeds[index].ID;
+            var request = _descriptionRequestFactory.Create(id, DescriptionReceivedEventHandler);
+            _requestQueue.Add(request);
+        }
+
+        private void DescriptionReceivedEventHandler(DogBreedDescriptionRequest request)
+        {
+            
         }
 
         private void DogBreadsReceivedEventHandler(DogBreedsRequest request)
