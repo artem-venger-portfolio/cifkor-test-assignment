7981167e1eee082f6acdd44ac4ada58a2854e344
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 08:41:15 +0700

    Create DogBreedEntry

---
+++ Assets/WebClient/Code/UI/Screens/DogBreedsScreen/View/DogBreedEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WebClient
+{
+    public class DogBreedEntry : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _numberField;
+
+        [SerializeField]
+        private TMP_Text _breedNameField;
+
+        [SerializeField]
+        private Button _button;
+
+        private int _index;
+
+        private void Start()
+        {
+            _button.onClick.AddListener(ButtonClickedEventHandler);
+        }
+
+        public event Action<int> Clicked;
+
+        public void SetIndex(int index)
+        {
+            _index = index;
+            var number = _index + 1;
+            _numberField.text = number.ToString();
+        }
+
+        public void SetName(string breedName)
+        {
+            _breedNameField.text = breedName;
+        }
+
+        private void ButtonClickedEventHandler()
+        {
+            Clicked?.Invoke(_index);
+        }
+    }
+}
