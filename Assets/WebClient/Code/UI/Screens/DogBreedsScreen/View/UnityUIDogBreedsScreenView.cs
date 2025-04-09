8162d319c8c6e1f9132931bae0545738314dfac0
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 08:51:18 +0700

    Create breed entries

--- Assets/WebClient/Code/UI/Screens/DogBreedsScreen/View/UnityUIDogBreedsScreenView.cs
+++ Assets/WebClient/Code/UI/Screens/DogBreedsScreen/View/UnityUIDogBreedsScreenView.cs
@@ -11,12 +11,28 @@ namespace WebClient
 
         [SerializeField]
         private DogBreedEntry _entryTemplate;
-        
+
+        private readonly List<DogBreedEntry> _entries = new();
+
         public event Action Shown;
         public event Action Hidden;
 
         public void DisplayBreeds(IReadOnlyList<DogBreedShortInfo> breeds)
         {
+            for (var i = 0; i < breeds.Count; i++)
+            {
+                if (i >= _entries.Count)
+                {
+                    var newEntry = Instantiate(_entryTemplate, _content);
+                    newEntry.Clicked += EntryClickedEventHandler;
+                    _entries.Add(newEntry);
+                }
+
+                var currentBreed = breeds[i];
+                var currentEntry = _entries[i];
+                currentEntry.SetIndex(i);
+                currentEntry.SetName(currentBreed.Name);
+            }
         }
 
         protected override void OnShow()
@@ -26,7 +42,16 @@ namespace WebClient
 
         protected override void OnHide()
         {
+            foreach (var currentEntry in _entries)
+            {
+                currentEntry.gameObject.SetActive(value: false);
+            }
+
             Hidden?.Invoke();
+        }
+
+        private void EntryClickedEventHandler(int index)
+        {
         }
     }
 }
