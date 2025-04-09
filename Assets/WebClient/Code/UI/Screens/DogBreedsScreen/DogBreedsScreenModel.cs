83b7e54b4402d863819add7a0e046c7661ef1ffb
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 08:17:53 +0700

    Send and interrupt DogBreads request

--- Assets/WebClient/Code/UI/Screens/DogBreedsScreen/DogBreedsScreenModel.cs
+++ Assets/WebClient/Code/UI/Screens/DogBreedsScreen/DogBreedsScreenModel.cs
@@ -2,12 +2,34 @@
 {
     public class DogBreedsScreenModel
     {
+        private readonly DogBreedsRequest.Factory _dogBreedsRequestFactory;
+        private readonly IRequestQueue _requestQueue;
+        private readonly IProjectLogger _logger;
+
+        public DogBreedsScreenModel(DogBreedsRequest.Factory dogBreedsRequestFactory, IRequestQueue requestQueue,
+                                    IProjectLogger logger)
+        {
+            _dogBreedsRequestFactory = dogBreedsRequestFactory;
+            _requestQueue = requestQueue;
+            _logger = logger;
+        }
+
         public void GetBreeds()
         {
+            _requestQueue.Add(_dogBreedsRequestFactory.Create(DogBreadsReceivedEventHandler));
         }
 
         public void InterruptRequestsIfNeeded()
         {
+            _requestQueue.Interrupt(RequestType.DogBreeds);
+        }
+
+        private void DogBreadsReceivedEventHandler(DogBreedsRequest request)
+        {
+            foreach (var info in request.Result)
+            {
+                _logger.LogInfo($"name: {info.Name}, id: {info.ID}");
+            }
         }
     }
 }
