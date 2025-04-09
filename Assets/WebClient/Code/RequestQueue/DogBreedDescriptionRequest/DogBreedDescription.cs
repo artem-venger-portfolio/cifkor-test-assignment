2f46344ebde9f286fef7286c25ad6aff71140eeb
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 09:17:40 +0700

    Create result type with description

---
+++ Assets/WebClient/Code/RequestQueue/DogBreedDescriptionRequest/DogBreedDescription.cs
@@ -0,0 +1,14 @@
+﻿namespace WebClient
+{
+    public readonly struct DogBreedDescription
+    {
+        public DogBreedDescription(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+    }
+}
