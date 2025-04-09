f7fe8f98dcea46286adb5863e3547a0cfcf5bdad
Venger Artem <artem.venger.professional@gmail.com>
2025-04-09 08:06:52 +0700

    Send DogBreedsRequest and receive response

--- Assets/WebClient/Code/RequestQueue/DogBreedsRequest/DogBreedsRequest.cs
+++ Assets/WebClient/Code/RequestQueue/DogBreedsRequest/DogBreedsRequest.cs
@@ -10,16 +10,21 @@ namespace WebClient
     [UsedImplicitly]
     public class DogBreedsRequest : IWebRequest
     {
+        private const string URL = "https://dogapi.dog/docs/api-v2/breeds";
         private readonly MonoBehaviourFunctions _monoBehaviourFunctions;
         private readonly Action<DogBreedsRequest> _completed;
+        private readonly IProjectLogger _logger;
+        private DogBreedsResponse _response;
         private Coroutine _requestCoroutine;
         private UnityWebRequest _request;
         private bool _isInProgress;
 
-        public DogBreedsRequest(MonoBehaviourFunctions monoBehaviourFunctions, Action<DogBreedsRequest> completed)
+        public DogBreedsRequest(MonoBehaviourFunctions monoBehaviourFunctions, Action<DogBreedsRequest> completed,
+                                IProjectLogger logger)
         {
             _monoBehaviourFunctions = monoBehaviourFunctions;
             _completed = completed;
+            _logger = logger;
         }
 
         public RequestType Type => RequestType.DogBreeds;
@@ -46,7 +51,7 @@ namespace WebClient
                 _monoBehaviourFunctions.KillCoroutine(_requestCoroutine);
                 _requestCoroutine = null;
             }
-            
+
             _request.Dispose();
             _request = null;
         }
@@ -55,7 +60,20 @@ namespace WebClient
         {
             _isInProgress = true;
 
-            yield return null;
+            _request = UnityWebRequest.Get(URL);
+
+            yield return _request;
+
+            if (_request.result != UnityWebRequest.Result.Success)
+            {
+                _logger.LogError(message: "DogBreedsRequest failed!");
+                _requestCoroutine = null;
+                _isInProgress = false;
+                yield break;
+            }
+
+            var json = _request.downloadHandler.text;
+            _response = JsonUtility.FromJson<DogBreedsResponse>(json);
 
             _requestCoroutine = null;
             _isInProgress = false;
